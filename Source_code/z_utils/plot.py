fbd8a31f0c0aa0098a33238d3aa27d2775673fd4
marcel8168 <marcel_hiltner@protonmail.com>
2024-03-15 17:10:07 +0100

    [utils] add plot function

---
+++ Source_code/z_utils/plot.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt
+
+
+def plot(data_list: list, data_label_list: list[str], title, ylabel, xlabel, vlines: list = [], ylim=None, model_name: str = None):
+    for i, data in enumerate(data_list):
+        plt.plot(data, label=data_label_list[i])
+    for i, vline in enumerate(vlines):
+        plt.axvline(x=vline, color="tab:orange", linestyle="dotted",
+                    label="End of epoch" if i == 0 else None)
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    if ylim:
+        plt.ylim(ylim)
+    if len(data_list) > 1 or len(vlines) > 0:
+        plt.legend()
+    plt.savefig(
+        f"{title}_{model_name[model_name.find('/')+1:]}.pdf", format="pdf", bbox_inches="tight")
+    plt.show()
+    plt.close()
