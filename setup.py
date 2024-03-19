05a700e4e869451a2c5b90058c4fdaa4b2ded675
marcel8168 <marcel_hiltner@protonmail.com>
2024-03-19 19:59:33 +0100

    [chore] automatically detect packages

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='Appendix',
+setup(name='appendix',
       version='0.1',
       description="Appendix of Master's Thesis: Classification of medical texts to differentiate between human medical and veterinary subjects.",
       url='https://github.com/marcel8168/MedTextClassification',
       author='Marcel Hiltner',
       author_email='marcel.hiltner@fau.de',
       license='MIT',
-      packages=['Source_code']
+      packages=find_packages()
       )
